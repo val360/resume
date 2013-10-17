6534a64409deba1bb0d224e237028c191ca915a9
Valentin Avksentyev <avksentyev@gmail.com>
2013-10-17 03:28:23 -0400

    initial commit

---
+++ src/main/java/net/val360/HomePage.java
@@ -0,0 +1,15 @@
+package net.val360;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		// add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+
+    }
+}
